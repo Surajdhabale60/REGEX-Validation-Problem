923857dfac73aae12687c2ff17c37dcb1ddb29e8
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-30 20:54:24 +0530

    [Suraj]Add:Checking By Entering Alphabet At Beginning

--- RegexValidateProblem/RegexValidateProblem/PostalCode.cs
+++ RegexValidateProblem/RegexValidateProblem/PostalCode.cs
@@ -12,9 +12,21 @@ namespace RegexValidateProblem
         public class PostalCode
         {
             const string PIN_CODE_PATTERN = "^[0-9]{6}$";
+            const string PIN_CODE_ATBEGINNING = "^[0-9]{7}$";
             public void TestPinCode(string input)
             {
                 if (Regex.IsMatch(input, PIN_CODE_PATTERN))
+                {
+                    Console.WriteLine("Pin code matches");
+                }
+                else
+                {
+                    Console.WriteLine("Pin code failed");
+                }
+            }
+            public void AlphabetAtBeginning(string input)
+            {
+                if (Regex.IsMatch(input, PIN_CODE_ATBEGINNING))
                 {
                     Console.WriteLine("Pin code matches");
                 }
--- RegexValidateProblem/RegexValidateProblem/Program.cs
+++ RegexValidateProblem/RegexValidateProblem/Program.cs
@@ -10,15 +10,21 @@ namespace RegexValidateProblem
             while(flag)
             {
                 Console.WriteLine("Enter option");
-                Console.WriteLine("1.Postal Code");
+                Console.WriteLine(" 1.Postal Code\n 2.Taking Alphabet At Begninning");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
                     case 1:
                         PostalCode postalCode = new PostalCode();
-                        Console.WriteLine("Enter PostalCode");
+                        Console.WriteLine("Enter Postal Code");
                         string code = Convert.ToString(Console.ReadLine());
                         postalCode.TestPinCode(code);
+                        break;
+                    case 2:
+                        PostalCode postalCode1 = new PostalCode();
+                        Console.WriteLine("Enter PostalCode");
+                        string code1 = Convert.ToString(Console.ReadLine());
+                        postalCode1.AlphabetAtBeginning(code1);
                         break;
                 }
             }
