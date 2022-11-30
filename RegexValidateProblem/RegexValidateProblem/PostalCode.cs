b067f376af7ede02f8f9844eef9e94bef7113b3d
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-30 21:03:18 +0530

    [Suraj]Add:Checkin By Entering Alphabet At End

--- RegexValidateProblem/RegexValidateProblem/PostalCode.cs
+++ RegexValidateProblem/RegexValidateProblem/PostalCode.cs
@@ -13,6 +13,7 @@ namespace RegexValidateProblem
         {
             const string PIN_CODE_PATTERN = "^[0-9]{6}$";
             const string PIN_CODE_ATBEGINNING = "^[0-9]{7}$";
+            const string Pin_Code_AtEnd = "^[0-9]{7}$";
             public void TestPinCode(string input)
             {
                 if (Regex.IsMatch(input, PIN_CODE_PATTERN))
@@ -27,6 +28,17 @@ namespace RegexValidateProblem
             public void AlphabetAtBeginning(string input)
             {
                 if (Regex.IsMatch(input, PIN_CODE_ATBEGINNING))
+                {
+                    Console.WriteLine("Pin code matches");
+                }
+                else
+                {
+                    Console.WriteLine("Pin code failed");
+                }
+            }
+            public void AlphabetAtEnd(string input)
+            {
+                if (Regex.IsMatch(input, Pin_Code_AtEnd))
                 {
                     Console.WriteLine("Pin code matches");
                 }
--- RegexValidateProblem/RegexValidateProblem/Program.cs
+++ RegexValidateProblem/RegexValidateProblem/Program.cs
@@ -10,7 +10,7 @@ namespace RegexValidateProblem
             while(flag)
             {
                 Console.WriteLine("Enter option");
-                Console.WriteLine(" 1.Postal Code\n 2.Taking Alphabet At Begninning");
+                Console.WriteLine(" 1.Postal Code\n 2.Taking Alphabet At Begninning\n 3.Taking Alphabet at End");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -25,6 +25,12 @@ namespace RegexValidateProblem
                         Console.WriteLine("Enter PostalCode");
                         string code1 = Convert.ToString(Console.ReadLine());
                         postalCode1.AlphabetAtBeginning(code1);
+                        break;
+                    case 3:
+                        PostalCode postalCode2 = new PostalCode();
+                        Console.WriteLine("Enter PostalCode");
+                        string code2 = Convert.ToString(Console.ReadLine());
+                        postalCode2.AlphabetAtBeginning(code2);
                         break;
                 }
             }
