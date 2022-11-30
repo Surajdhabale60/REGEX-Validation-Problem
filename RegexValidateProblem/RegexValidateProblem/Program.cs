e7f9ad51f3093a4459a10ef56f8ed5e4e5a7067b
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-30 20:30:54 +0530

    [Suraj]Add:Validate Poastal code

---
+++ RegexValidateProblem/RegexValidateProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using RegexValidateProblem.RegexDemo;
+
+namespace RegexValidateProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Enter option");
+                Console.WriteLine("1.Postal Code");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        PostalCode postalCode = new PostalCode();
+                        Console.WriteLine("Enter PostalCode");
+                        string code = Convert.ToString(Console.ReadLine());
+                        postalCode.TestPinCode(code);
+                        break;
+                }
+            }
+           
+        }
+    }
+}
